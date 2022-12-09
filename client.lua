8a08afccbd11f9ecc8c4a41a4d94d1f3a6ae818d
Leon Brunsmann <Leon.brunsmann@gmail.com>
2022-12-09 14:54:20 +0100

    Initial commit

---
+++ client.lua
@@ -0,0 +1,9 @@
+RegisterCommand('help', function()
+
+    TriggerEvent('chat:addMassage', {
+        color = {255, 0, 0}
+        mulitline = true,
+        args = {"Server", "Kommm doch mal auf unseren Discord Server Https://discord.gg/fivezero"}
+    })
+
+end, false) 
---
+++ fxmanifest.lua
@@ -0,0 +1,8 @@
+fx_version 'cerulean'
+game 'gta5'
+
+author 'AngeCloud'
+description 'First Script, Help Script'
+Version '1.0.0'
+
+client_script 'client.lua'
