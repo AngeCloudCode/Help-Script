ca61c7efe27bc4c57a8866f9def18c2fabb2c007
Leon Brunsmann <Leon.brunsmann@gmail.com>
2022-12-09 15:00:29 +0100

    Update client.lua

--- client.lua
+++ client.lua
@@ -3,7 +3,7 @@ RegisterCommand('help', function()
     TriggerEvent('chat:addMassage', {
         color = {255, 0, 0}
         mulitline = true,
-        args = {"Server", "Kommm doch mal auf unseren Discord Server Https://discord.gg/fivezero"}
+        args = {"Server", "Kommm doch mal auf unseren Discord Server https://discord.gg/fivezero"}
     })
 
 end, false) 
